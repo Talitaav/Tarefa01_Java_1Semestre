0239bab0fa675cbe076f7d0b13abee5976281cb9
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-02-27 20:31:43 -0300

    Update Exercicio11.java

--- Tarefa_01_Java_01_Semestre2024/Exercicio11.java
+++ Tarefa_01_Java_01_Semestre2024/Exercicio11.java
@@ -4,8 +4,8 @@ package exercicio;
 
 public class Exercicio11 {
 
-	//*11) Escreva um algoritmo para ler as dimensões de um trapézio (base amior, base menor e altura), calcular e escrever a área do trapézio. a=(B+b).h/2*\\
-	
+	//11) Escreva um algoritmo para ler as dimensões de um trapézio (base amior, base menor e altura), calcular e escrever a área do trapézio. a=(B+b).h/2
+
 	public static void main(String[]args) {
 		
 		System.out.println("Digite a base maior: ");
