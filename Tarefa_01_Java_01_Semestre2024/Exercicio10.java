6e594b7f91e7fc04ed4404af4059a53a7949f91d
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-02-27 20:31:18 -0300

    Update Exercicio10.java

--- Tarefa_01_Java_01_Semestre2024/Exercicio10.java
+++ Tarefa_01_Java_01_Semestre2024/Exercicio10.java
@@ -5,7 +5,7 @@ package exercicio;
 public class Exercicio10 {
 
 
-//*10) Escreva um algoritmo para ler as dimensões de um losango (diagonal maior, diagonal menor), calcular e escrever a área do losango. Dica a=D.d/2*\\
+//10) Escreva um algoritmo para ler as dimensões de um losango (diagonal maior, diagonal menor), calcular e escrever a área do losango. Dica a=D.d/2
 
 	static void main(String[]args) {
 	
